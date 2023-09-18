ad9325129dd7453e447267ef46dfdf82adac587f
harrybark <harrypmw@gmail.com>
2023-09-18 23:03:11 +0900

    #16 - 게시글 관련 view 테스트 작성
    우선 어떤 경로로 뷰를 열어줄지 테스트로 명세를 뽑는다.

---
+++ src/main/java/com/daylog/tomatolog/controller/ArticleController.java
@@ -0,0 +1,11 @@
+package com.daylog.tomatolog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+
+}
