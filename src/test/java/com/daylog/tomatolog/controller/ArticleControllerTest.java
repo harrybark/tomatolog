c0bcb28357dc9937e7f4e7458eb34d09f2cde56e
harrybark <harrypmw@gmail.com>
2024-01-17 23:09:58 +0900

    #16 - Article Controller Test Update MediaType 의 부가적인 옵션을 확인하기위해 모두 변경함. af3a204 에서 확인함.

--- src/test/java/com/daylog/tomatolog/controller/ArticleControllerTest.java
+++ src/test/java/com/daylog/tomatolog/controller/ArticleControllerTest.java
@@ -46,7 +46,7 @@ class ArticleControllerTest {
         // when &  then
         mockMvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -62,7 +62,7 @@ class ArticleControllerTest {
         // when &  then
         mockMvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search"));
     }
 
@@ -76,7 +76,7 @@ class ArticleControllerTest {
         // when &  then
         mockMvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search-hashtag"));
     }
 }
