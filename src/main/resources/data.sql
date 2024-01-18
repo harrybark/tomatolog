894798f8584aba874df64e87bcbbd131f9944e23
harrybark <harrypmw@gmail.com>
2024-01-19 00:42:29 +0900

    #16 - 유저 정보 관련

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
+insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at,
+                          modified_by)
+values ('harry', 'test1234', 'harry', 'devharrypmw@mail.com', 'I am Harry.', now(), 'harry', now(), 'harry')
+;
+
 -- 123 게시글
 insert into article (title, content, hashtag, created_by, modified_by, created_at, modified_at) values
                                                                                                     ('Quisque ut erat.', 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.
